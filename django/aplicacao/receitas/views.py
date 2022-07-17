6fd5247fda7641103aaeb84ac78f875ec0dbb50c
Ecrb3 <73610715+Ecrb3@users.noreply.github.com>
2022-07-17 02:03:16 -0300

    Aula final curso django

--- django/aplicacao/receitas/views.py
+++ django/aplicacao/receitas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.http import HttpResponse
 from .models import Receita
 
+
 def index(request):
 
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
