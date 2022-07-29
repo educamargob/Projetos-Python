dec4276afe77986c42938bd22ffca3ae9e6eb76b
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2022-07-29 16:42:30 -0300

    Getters e setters

--- python/OO/conta.py
+++ python/OO/conta.py
@@ -1,17 +1,38 @@
 class Conta:
-
-    def __int__(self, numero, titular, saldo, limite = 1000):
+    def __init__(self, numero, titular, saldo, limite = 1000.00):
         print("Construindo objeto ...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo {self.saldo} do titular {self.titular}")
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.saca(valor)  ##Saca valor da própria conta
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def set_limite(self, limite):
+        self.__limite = limite
+
+
+
+
