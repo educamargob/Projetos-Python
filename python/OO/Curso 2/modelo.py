538706155537826d0a697aca57736e13312496b8
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2022-08-02 16:35:05 -0300

    Aulas POO

---
+++ python/OO/Curso 2/modelo.py
@@ -0,0 +1,47 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def dar_like(self):
+        self._likes += 1
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+
+vingadores = Filme('vingadores', 2018, 160)
+vingadores.dar_like()
+print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2017, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} - {programa.likes}')
