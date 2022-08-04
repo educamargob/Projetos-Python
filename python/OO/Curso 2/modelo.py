8e14e8ca0badd0a97799280ff361a60bfa40b48c
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2022-08-04 16:44:30 -0300

    polimorfismo

--- python/OO/Curso 2/modelo.py
+++ python/OO/Curso 2/modelo.py
@@ -19,10 +19,16 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes}')
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} - min {self._likes} - likes ')
 
 
 class Serie(Programa):
@@ -30,18 +36,21 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} - temporadas {self._likes} - likes')
+
 
 vingadores = Filme('vingadores', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
+
 
 atlanta = Serie('atlanta', 2017, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} - {programa.likes}')
+    programa.imprime()
+
+
