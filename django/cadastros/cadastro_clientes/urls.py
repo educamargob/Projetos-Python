a35cb7d70424f2868f3afc04dc1155197e8c3dff
Ecrb3 <73610715+Ecrb3@users.noreply.github.com>
2022-08-20 23:18:55 -0300

    Ajustes projeto

--- django/cadastros/cadastro_clientes/urls.py
+++ django/cadastros/cadastro_clientes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('cadastro_clientes', views.cadastro_clientes, name='cadastro_clientes'),
     path('cria/cliente', views.cria_cliente, name='cria_cliente'),
     path('clientes', views.mostra_cliente, name='mostra_cliente'),
+    path('clientes/listar', views.lista_clientes, name='lista_clientes'),
 ]
--- django/cadastros/cadastro_clientes/views.py
+++ django/cadastros/cadastro_clientes/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, get_list_or_404, redirect
 from django.http import HttpResponse
 from cadastro_clientes.models import Cliente
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import *
 
 def index(request):
     return render(request, 'index.html')
+
+def lista_clientes(request):
+    clientes = Cliente.objects.order_by('-data_criacao')
+    paginator = Paginator(clientes, 10)
+    page = request.GET.get('page')
+    clientes_por_pagina = paginator.get_page(page)
+    dados = {
+        'clientes' : clientes_por_pagina
+    }
+    return render(request, 'clientes/listar_clientes.html', dados)
 
 def cadastro_clientes(request):
     form = ClientesForms()
@@ -23,10 +34,12 @@ def cria_cliente(request):
         cnpj = request.POST['cnpj']
         cliente = Cliente.objects.create(nome_cliente=nome_cliente, endereco=endereco, bairro=bairro, cidade=cidade, cep=cep, cnpj=cnpj)
         cliente.save()
-        return redirect('index')
+        return redirect('lista_clientes')
 
 def mostra_cliente(request):
     if request.method == 'POST':
         form = ClientesForms(request.POST)
         contexto = {'form':form}
         return render(request, 'clientes/mostra_cliente.html', contexto)
+
+
