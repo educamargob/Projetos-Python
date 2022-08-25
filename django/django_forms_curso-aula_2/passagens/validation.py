1fe176f1ec0f71b491d8a009ac4ebb515fcbb2a7
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2022-08-25 16:31:33 -0300

    Adicionando validações forms

--- django/django_forms_curso-aula_2/passagens/validation.py
+++ django/django_forms_curso-aula_2/passagens/validation.py
@@ -7,3 +7,14 @@ def campo_tem_numero(valor_campo, nome_campo, lista_de_erros):
     """Verifica se possui algum digito numerico"""
     if any(char.isdigit() for char in valor_campo):
             lista_de_erros[nome_campo] = 'Não inclua números nesse campo'
+
+def data_ida_maior_data_volta(data_ida, data_volta, lista_de_erros):
+    """Verifica se data de ida é maior que data de volta"""
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser maior que data de ida'
+
+def data_ida_menor_data_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """Verifica se a data de ida é menor que a data de hoje"""
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor hoje'
+
